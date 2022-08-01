fc77b8e06507d1978f0658635edc21db81d91b67
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-08-01 16:47:43 +0900

    defineで、ポートをいちいち指定

--- LGIC_IC_check.c
+++ LGIC_IC_check.c
@@ -11,20 +11,37 @@
 #include "fucntions.h"
 #include "lcdlib_xc8_v03.h"
 
-
+#define HOGE (A,2)
 
 #define DFF_Q1 PORTAbits.RA0
 #define DFF_nQ1 PORTAbits.RA1
 #define DFF_Q2 PORTAbits.RA2
 #define DFF_nQ2 PORTAbits.RA3
+#define DFF_COMD LATAbits.LA4
+#define DFF_COMCLR LATAbits.LA6
+
+#define CO1_Q2 DFF_Q1
+#define CO1_Q3 DFF_nQ1
+#define CO2_Q2 DFF_Q2
+#define CO2_Q3 DFF_nQ2
+#define CO_CLR DFF_COMD
+#define CO_CLK DFF_COMCLR
+
+
+#define DFF_CL1 LAT(A,3)
+#define DFF_PR1 LAT(A,4)
+#define DFF_CL2 LAT(A,5)
+#define DFF_PR2 LAT(A,6)
+
+#define CO1_Q0 PORT(A,3)
+#define CO1_Q1 PORT(A,3)
+#define CO2_Q0 PORT(A,3)
+#define CO2_Q1 PORT(A,3)
+
+
 #define DFF_CHECK(b) \
     if(DFF_Q1 != b || DFF_nQ1 != (!b) || DFF_Q2 != b || DFF_nQ2 != (!b))  return NG;
-#define DFF_D1 LATAbits.LA4
-#define DFF_D2 LATAbits.LA5
-#define DFF_CLR LATAbits.LA6
-#define DRR_PRE LATAbits.LA7
 
-#define CLOCK(a) a = 1; __delay_ms(PLUS_TIME); a = 0;
 
 extern unsigned char bitPattern[8]; //添え字の数のビットのみが1になったデータ配列
 /*
@@ -38,7 +55,7 @@ CHECK_RESULT dff_Check(){
         LATA = 0b00000101;  //CLR.PRCを1(off)に
         downClock(A, 2);        //プリセット端子を入れてHで初期化
         DFF_CHECK(1)
-        CLOCK(DFF_CLR)
+        CLOCK(DFF_COMCLR);
         
         downClock(A, 0);       //クリア端子を0に
         __delay_ms(WAIT_TIME);
@@ -49,15 +66,13 @@ CHECK_RESULT dff_Check(){
         DFF_CHECK(1)
                 
         /* D端子を0にしてクロック */
-        DFF_D1 = 0;
-        DFF_D2 = 0;
+        DFF_COMD = 0;
         clock(A, 3);
         __delay_ms(WAIT_TIME);
         DFF_CHECK(0)
         
         /* D端子を1にしてクロック */
-        DFF_D1 = 1;
-        DFF_D2 = 1;
+        DFF_COMD = 1;
         clock(A, 3);
         __delay_ms(WAIT_TIME);
         DFF_CHECK(1)
@@ -65,3 +80,12 @@ CHECK_RESULT dff_Check(){
         return OK;
 }
 
+
+CHECK_RESULT count_check(){
+    
+}
+
+int num_check(){
+    int num;
+    
+}
